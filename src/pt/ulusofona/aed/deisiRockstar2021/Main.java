a69d1a772c38d766d96ee14b2df12188734bba78
Telmo Panaças <telmoalexandre2505@gmail.com>
2021-05-27 17:03:49 +0100

    teste net

--- src/pt/ulusofona/aed/deisiRockstar2021/Main.java
+++ src/pt/ulusofona/aed/deisiRockstar2021/Main.java
@@ -51,6 +51,7 @@ public class Main {
 
 
 
+
         System.out.println("----------------------LEITURA DO FICHEIRO song_details.txt------------");
 
         long start3 = System.currentTimeMillis();
